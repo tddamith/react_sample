5835af70808e74b9bae4c59708581489ce1422ec
damith <dpremakumara@gmail.com>
2025-10-07 22:22:23 +0530

    initial style setup

---
+++ src/components/test/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+
+const Test = ({ data, onClick }) => {
+  return (
+    <>
+      <div className="cw__verify-wrapper">
+
+      </div>
+    </>
+  );
+};
+export default Test;
