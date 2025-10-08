f8d7860087344f289fda080f90156f7eb0b91713
Hirusha Pathiraja <hirushadspathiraja@gmail.com>
2025-10-08 12:42:10 +0530

    created_input_field

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,10 +2,6 @@ import TestComponent from "./testMenu";
 import DropDownComponent from "./dropDown";
 import SingIn from "./signIn/signin";
 import Button from "./button/button";
+import InputField from "./input_field/input_filed";
 
-export {
-  TestComponent,
-  DropDownComponent,
-  SingIn,
-  Button
-};
+export { TestComponent, DropDownComponent, SingIn, Button, InputField };
