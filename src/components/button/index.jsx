90878475ed8522415338c65f5c7cbf9deb3fbf61
yasasArt <yasasjayaweera945@gmail.com>
2025-10-14 10:39:09 +0530

    updated button

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./button.scss";
 
-const Button = (props) => {
-    return (
-        <button className={`gradient-btn btn-primary ${props?.type} ${props?.size} `} onClick={props.onClick}>
-            {props?.text}
-        </button>
-    );
+const Button = ({ text, type, size, disabled, onClick }) => { // i use this prop to handle click events
+  const buttonType = disabled ? "disabled" : type;
+  return (
+    <button
+      className={`btn-primary ${buttonType} ${size}`}
+      onClick={!disabled ? onClick : undefined}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 };
-
-
-
-
 
 export default Button;
